da9943f786f4c7cf05a3103353682bdf079e4bf0
Jacques <jacco.amersfoort@gmail.com>
2024-07-15 23:01:58 +0200

    Combine interval coordinates between TO and FROM coordinate pairs
    When TO has the same interval as FROM, they must become the same coordinate at that position

--- generate.php
+++ generate.php
@@ -47,18 +47,18 @@ class Generate
 			$this->commands[] = PHP_EOL . "## " . $label;
 		}
 		// Split both coordinates into subcommands if required
-		foreach ($this->subCoords($from) as $subFrom) {
-			// Iterate same method again but with newly adjusted 
-			// Don't pass along Label to group interval commands under group label
-			$this->fillBlocks(from: $subFrom, block: $block, to: $to);
-		}
-		// TO coordinates should be combined with FROM coordinates into one
-		// TODO combine TO coordinates with FROM coordinates to enable creating a grid for example
-		if ($to && ($subTos = $this->subCoords($to))) {
-			throw new \Exception("Interval coordinates at `to` not supported; (" . var_export($to[2], true) . "), simply add the limit of the FROM coordinates");
+		foreach ($this->subCoords($from, $to) as $subFrom) {
+			// Iterate same method again but with newly adjusted subfrom
+			// When subcoord contains 'to', it was edited for interval
+			if (!empty($subFrom['to'])) {
+				// In this case, method returned special array with 'from' and 'to' keys separately
+				$this->fillBlocks(from: $subFrom['from'], block: $block, to: $subFrom['to']);
+			} else {
+				// Don't pass along Label to group interval commands under group label
+				$this->fillBlocks(from: $subFrom, block: $block, to: $to);
+			}
 		}
 
-		//var_dump("Second iteration with args: ", func_get_args());
 		$command = 'fill';
 
 		// Add FROM coordinates
@@ -72,7 +72,6 @@ class Generate
 		$command .= ' minecraft:' . $block;
 
 		$this->commands[] = $command;
-		//var_dump("Added command `" . $command . "` to commands array", $this->commands);die;
 	}
 
 	// Convert coordinates array into string
@@ -92,25 +91,47 @@ class Generate
 		return $coord;
 	}
 
-	protected function subCoords(array $coords)
+	protected function subCoords(array $coords, ?array $to = null)
 	{
 		$subCoords = [];
-		foreach ($coords as $pos => $fr) {
-			if (str_contains(haystack: $fr, needle: "/")) {
+		foreach ($coords as $pos => $intervalCoord) {
+			if (str_contains(haystack: $intervalCoord, needle: "/")) {
 				// This coordinate at $post contains a slash, divide into steps
 				// Create new $from and $to
 				$subFrom = $coords;
-				$parts = explode("/", $fr);
+				$parts = explode("/", $intervalCoord);
 				$limit = $parts[0]; // Limit is before slash
 				$stepSize = $parts[1]; // Step size is after slash
 				if ($stepSize > $limit) {
-					throw new Exception("Invalid coordinates, stepsize `" . $stepSize . "` may not be larger than limit `" . $limit . "`");
+					throw new Exception("Invalid coordinates: stepsize `" . $stepSize . "` may not be larger than limit `" . $limit . "`");
 				}
 				// TODO if $limit is negative, loop does not run
 				for ($i = 0; $i < $limit; $i += $stepSize) {
-					// Overwrite from coordinate with simple line here
+					// Overwrite FROM coordinate with simple line here
 					$subFrom[$pos] = $i;
-					$subCoords[] = $subFrom;
+
+					/** Combine FROM with TO intervals */
+					if ($to) {
+						// And TO has interval
+						if (str_contains(haystack: $to[$pos], needle: "/")) {
+							// And TO has the same interval at the same position
+							if ($to[$pos] == $intervalCoord) {
+								// Update TO coordinates as well
+								$subTo = $to;
+								$subTo[$pos] = $i;
+
+								// When TO must be updated, return special array
+								$subCoords[] = [
+									'from' => $subFrom,
+									'to' => $subTo,
+								];
+							}
+						} else {
+							throw new Exception("Cannot combine FROM and TO interval coordinates! From: " . var_export($subFrom, true) . "; To: " . var_export($to, true));
+						}
+					} else {
+						$subCoords[] = $subFrom;
+					}
 				}
 			}
 		}
