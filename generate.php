a743ca0eff1223b618f53ae0df76fab3cca4c586
Jacques <jacco.amersfoort@gmail.com>
2024-07-15 20:34:55 +0200

    Generate a list of fill commands with PHP Array
    use class and properties to build list of commands and print afterwards

--- generate.php
+++ generate.php
@@ -1,7 +1,7 @@
 <?php
 $blocks = [
 	[
-		// Place this block on every 10th block until 100
+		// Place this block until 100
 		'from' => [1, 0, '100'],
 		'to' => null,
 		'block' => 'redstone_torch',
@@ -14,49 +14,65 @@ $blocks = [
 	]
 
 ];
+// Run
+$generator = new Generate();
+$generator->run($blocks);
 
-foreach ($blocks as $block) {
-	//$stepSize = 10;
-	//for ($y = 0; $y < 10; $y++) {
-	//	$ycoord = $y ? $y * $stepSize : null;
-		// Flip for negative (north)
-	//	$ycoord *= -1;
-		echo fillBlocks(...$block);
-	//}
-}
 
-/** Return a /fill command for blocks at coordinates
- *
- * @param $from - array coordinates start
- * @param $block - Block name
- * @param $to - array coordinates end (if emtpy, $from is used to just set)
- */
-function fillBlocks($from, $block, $to = null)
+class Generate
 {
-	$command = 'fill';
+	protected array $commands = [];
+	public function run(array $blocks)
+	{
 
-	// Add from coordinates
-	$command .= coords($from);
-
-	// if $to was passed, use it, otherwise reuse $from to place one block
-	$command .= coords($to ?? $from);
-
-	// Add block (all blocks have this prefix)
-	$command .= ' minecraft:' . $block;
-
-	return $command . PHP_EOL;
-}
-
-// Convert coordinates array into string
-function coords(array $coords)
-{
-	$coord = '';
-	foreach ($coords as $c) {
-		$coord .= ' ~'; // Always start at user
-		// Skip zero
-		if ($c) {
-			$coord .= $c;
+		foreach ($blocks as $block) {
+			$this->fillBlocks(...$block);
 		}
+
+		// Echo each command
+		array_walk($this->commands, function ($command) {
+			echo $command . PHP_EOL;
+		});
 	}
-	return $coord;
+
+	/** Return a /fill command for blocks at coordinates
+	 *
+	 * @param $from - array coordinates start
+	 * @param $block - Block name
+	 * @param $to - array coordinates end (if emtpy, $from is used to just set)
+	 */
+	protected function fillBlocks($from, $block, $to = null)
+	{
+		$command = 'fill';
+
+		// Add FROM coordinates
+		$command .= $this->coords($from);
+
+		// Add TO coordinates
+		// if $to was passed, use it, otherwise reuse $from to place one block
+		$command .= $this->coords($to ?? $from);
+
+		// Add block (all blocks have this prefix)
+		$command .= ' minecraft:' . $block;
+
+		$this->commands[] = $command;
+		//var_dump("Added command `" . $command . "` to commands array", $this->commands);die;
+	}
+
+	// Convert coordinates array into string
+	protected function coords(array $coords)
+	{
+		$coord = '';
+		if (sizeof($coords) < 3) {
+			throw new Exception("Invalid Coordinates!");
+		}
+		foreach ($coords as $c) {
+			$coord .= ' ~'; // Always start at user
+			// Skip zero
+			if ($c) {
+				$coord .= $c;
+			}
+		}
+		return $coord;
+	}
 }
