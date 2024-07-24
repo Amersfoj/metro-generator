c793accb9f31cdb6156b1003d9ccd0580eba76ef
Jacco Amersfoort <jacco.amersfoort@gmail.com>
2024-07-24 22:42:45 +0200

    Remove unused Imports

--- app/Filament/Resources/DatapackResource/Pages/CreateDatapack.php
+++ app/Filament/Resources/DatapackResource/Pages/CreateDatapack.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\DatapackResource\Pages;
 
 use App\Filament\Resources\DatapackResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateDatapack extends CreateRecord
--- app/Filament/Resources/FuncResource/Pages/CreateFunc.php
+++ app/Filament/Resources/FuncResource/Pages/CreateFunc.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\FuncResource\Pages;
 
 use App\Filament\Resources\FuncResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateFunc extends CreateRecord
