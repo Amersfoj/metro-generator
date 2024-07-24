1a9804f25cf3857d1ebde988b33adde742be3fec
Jacco Amersfoort <jacco.amersfoort@gmail.com>
2024-07-24 21:14:54 +0200

    Add Fillable arrays to both models
    Enable saving model

--- app/Models/Func.php
+++ app/Models/Func.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Func extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "description",
+        'pack_id',
+    ];
 }
