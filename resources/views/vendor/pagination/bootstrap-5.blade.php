c97c104619605c6dbc969ff2a1a2b37b8ad9ef4d
Jacco Amersfoort <jacco.amersfoort@gmail.com>
2024-07-24 20:26:23 +0200

    Format everything
    Run autoformatting on everything

--- resources/views/vendor/pagination/bootstrap-5.blade.php
+++ resources/views/vendor/pagination/bootstrap-5.blade.php
@@ -25,7 +25,6 @@
                 @endif
             </ul>
         </div>
-
         <div class="d-none flex-sm-fill d-sm-flex align-items-sm-center justify-content-sm-between">
             <div>
                 <p class="small text-muted">
@@ -38,7 +37,6 @@
                     {!! __('results') !!}
                 </p>
             </div>
-
             <div>
                 <ul class="pagination">
                     {{-- Previous Page Link --}}
@@ -56,14 +54,16 @@
                     @foreach ($elements as $element)
                         {{-- "Three Dots" Separator --}}
                         @if (is_string($element))
-                            <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                            <li class="page-item disabled" aria-disabled="true">
+                                <span class="page-link">{{ $element }}</span></li>
                         @endif
 
                         {{-- Array Of Links --}}
                         @if (is_array($element))
                             @foreach ($element as $page => $url)
                                 @if ($page == $paginator->currentPage())
-                                    <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                                    <li class="page-item active" aria-current="page">
+                                        <span class="page-link">{{ $page }}</span></li>
                                 @else
                                     <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
                                 @endif
