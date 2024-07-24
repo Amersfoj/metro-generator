202cacdcea28610775677e5bed31319cc789969f
Jacco Amersfoort <jacco.amersfoort@gmail.com>
2024-07-24 21:48:58 +0200

    Add Line model with CRUD and Fillable

---
+++ app/Filament/Resources/LineResource.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LineResource\Pages;
+use App\Filament\Resources\LineResource\RelationManagers;
+use App\Models\Line;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LineResource extends Resource
+{
+    protected static ?string $model = Line::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('from')
+                    ->required()
+                    ->maxLength(32),
+                Forms\Components\TextInput::make('to')
+                    ->required()
+                    ->maxLength(32),
+                Forms\Components\TextInput::make('block')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('func_id')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('from')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('to')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('block')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('func_id')
+                    ->numeric()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLines::route('/'),
+            'create' => Pages\CreateLine::route('/create'),
+            'edit' => Pages\EditLine::route('/{record}/edit'),
+        ];
+    }
+}
